29be9185ee104dfd9317ce3a50cac64c16f87d1f
Dativerse <Dativerse@gmail.com>
2025-07-16 18:42:55 +0400

    add fail test CalculateScore_WithSpareFrames_ReturnsExpectedScore

--- BowlingTests/BowlingControllerTest.cs
+++ BowlingTests/BowlingControllerTest.cs
@@ -11,12 +11,22 @@ public class BowlingControllerTest
     {
         _controller = new BowlingGameController();
     }
-
    
     [Theory]
     [TestCase("54 45 45 45 45 45 45 45 45 45", 90, Description = "All normal frames")]
     [TestCase("9- 9- 81 9- 9- 9- 9- 9- 9- 9-", 90, Description = "All frames with 9 and miss")]
     public void CalculateScore_WithNormalFrames_ReturnsExpectedScore(string frames, int expectedScore)
+    {
+        // Act
+        var result = _controller.Calculate(frames);
+        
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedScore));
+    }
+    
+    [Theory]
+    [TestCase("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5-", 140, Description = "All spare frames and normal in final")]
+    public void CalculateScore_WithSpareFrames_ReturnsExpectedScore(string frames, int expectedScore)
     {
         // Act
         var result = _controller.Calculate(frames);
