e428060135e548dd7c6f5285d5ffc99e57019f32
Dativerse <Dativerse@gmail.com>
2025-07-16 17:31:42 +0400

    add first failed test

--- BowlingTests/BowlingControllerTest.cs
+++ BowlingTests/BowlingControllerTest.cs
@@ -8,9 +8,20 @@ public class BowlingControllerTest
     }
 
     [Test]
-    public void Test1()
+    public void normal_when_34_before_normal_frame()
     {
+        var service = new BowlingCalculatorService();
+        var result = service.Calculate("45 45 45 45 45 45 45 45 45 45");
         
+        Assert.That(result, Is.EqualTo(90));
     }
     
+}
+
+public class BowlingCalculatorService
+{
+    public int Calculate(string rawFrames)
+    {
+        throw new NotImplementedException();
+    }
 }
