faf4cac33ae60248fbb311331fac7da9934d1a9d
Dativerse <Dativerse@gmail.com>
2025-07-17 11:03:58 +0400

    add failed test All strike frames

--- BowlingTests/BowlingControllerTest.cs
+++ BowlingTests/BowlingControllerTest.cs
@@ -18,7 +18,10 @@ public class BowlingControllerTest
     [TestCase("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 54 5-", 134, Description = "All spare frames and normal in final")]
     [TestCase("X X 45 X X X X X 45 5-", 199, Description = "All strike frames and normal in final")]
     [TestCase("X X 45 X X 7/ X X 45 5-", 176, Description = "All strike frames with spare and normal in final")]
-    [TestCase("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 54 5/5", 144, Description = "Spare in final with normal behind")]
+    [TestCase("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 54 5/5", 144, Description = "Spare frame in final with normal frame behind")]
+    [TestCase("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5", 150, Description = "All spare frames")]
+    [TestCase("X X X X X X X X X X X X", 300, Description = "All strike frames")]
+    
     public void CalculateScore_WithFrames_ReturnsExpectedScore(string frames, int expectedScore)
     {
         // Act
