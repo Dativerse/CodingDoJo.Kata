a892e8be61e57ea66a5719c296f9c37846978618
Dativerse <Dativerse@gmail.com>
2025-07-17 11:31:12 +0400

    refactor unit tests

--- BowlingTests/BowlingControllerTest.cs
+++ BowlingTests/BowlingControllerTest.cs
@@ -15,13 +15,13 @@ public class BowlingControllerTest
     [Theory]
     [TestCase("54 45 45 45 45 45 45 45 45 45", 90, Description = "All normal frames")]
     [TestCase("9- 9- 81 9- 9- 9- 9- 9- 9- 9-", 90, Description = "All frames with 9 and miss")]
-    [TestCase("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 54 5-", 134, Description = "All spare frames and normal in final")]
+    [TestCase("X X X 5- X X X X X XXX", 255, Description = "All strike frames with miss in the middle")]
+    [TestCase("X X X X X X X X X XXX", 300, Description = "All strike frames")]
     [TestCase("X X 45 X X X X X 45 5-", 199, Description = "All strike frames and normal in final")]
     [TestCase("X X 45 X X 7/ X X 45 5-", 176, Description = "All strike frames with spare and normal in final")]
+    [TestCase("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 54 5-", 134, Description = "All spare frames and normal in final")]
     [TestCase("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 54 5/5", 144, Description = "Spare frame in final with normal frame behind")]
     [TestCase("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5", 150, Description = "All spare frames")]
-    [TestCase("X X X X X X X X X XXX", 300, Description = "All strike frames")]
-    [TestCase("X X X 5- X X X X X XXX", 255, Description = "All strike frames with miss in the middle")]
     
     public void CalculateScore_WithFrames_ReturnsExpectedScore(string frames, int expectedScore)
     {
