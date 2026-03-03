c49594b365cba90dab0543673717442106453f35
Dativerse <Dativerse@gmail.com>
2026-03-03 15:41:13 +0700

    feat: optimize list to array

--- ProductsOfArrayExceptSelf/ProductsOfArrayExceptSelfTest.cs
+++ ProductsOfArrayExceptSelf/ProductsOfArrayExceptSelfTest.cs
@@ -3,10 +3,21 @@
 public class ProductsOfArrayExceptSelfTest
 {
     [Test]
-    public void have_value_when_receive_array()
+    public void have_value_when_receive_array_1()
     {
         int[] input = [1, 2, 4, 6];
-        var expected = new[] { 48,24,12,8 };
+        var expected = new[] { 48, 24, 12, 8 };
+
+        var result = ProductExceptSelf(input);
+        
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void have_value_when_receive_array_2()
+    {
+        int[] input = [-1, 0, 1, 2, 3];
+        var expected = new[] { 0, -6, 0, 0, 0 };
 
         var result = ProductExceptSelf(input);
         
@@ -15,40 +26,33 @@ public class ProductsOfArrayExceptSelfTest
 
     private int[] ProductExceptSelf(int[] nums)
     {
-        var leftToRight = new List<int>();
-        var rightToLeft = new List<int>();
-        
         if (nums.Length <= 1) {
             return nums;
         }
-
-        leftToRight.Add(nums[0]);
-        rightToLeft.Add(nums[^1]);
+        
+        var leftToRight = new int[nums.Length];
+        leftToRight[0] = nums[0];
+        
+        var rightToLeft = new int[nums.Length];
+        rightToLeft[0] = nums[^1];
 
         for (var i = 1; i < nums.Length; i++) {
-            leftToRight.Add(nums[i] * leftToRight[i - 1]);
-            rightToLeft.Add(nums[nums.Length - 1 - i] * rightToLeft[i - 1]);
+            leftToRight[i] = nums[i] * leftToRight[i - 1];
+            rightToLeft[i] = nums[nums.Length - 1 - i] * rightToLeft[i - 1];
         }
 
-        var result = new List<int>();
-        const int leftToRightIndex = 0;
-        var rightToLeftIndex = nums.Length - 1;
-        for (var i = 0; i < nums.Length; i++)
+        // pre-process with item at index 0
+        var result = new int[nums.Length];
+        result[0] = rightToLeft[^2];
+        
+        // merge LeftToRight storage with RightToLeft storage
+        for (var i = 1; i < nums.Length - 1; i++)
         {
-            if (i == leftToRightIndex)
-            {
-                result.Add(rightToLeft[^2]);
-                continue;
-            }
-
-            if (i == rightToLeftIndex)
-            {
-                result.Add(leftToRight[i - 1]);
-                continue;
-            }
-            
-            result.Add(leftToRight[i - 1] * rightToLeft[rightToLeft.Count - 2 - i]);
+            result[i] = leftToRight[i - 1] * rightToLeft[rightToLeft.Length - 2 - i];
         }
+        
+        // post-process with item at last index
+        result[nums.Length - 1] = leftToRight[nums.Length - 2];
 
         return result.ToArray();
     }
