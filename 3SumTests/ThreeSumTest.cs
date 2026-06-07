1a3c72baaa6b5559d125b7ac7b311706e9278fa4
Dativerse <Dativerse@gmail.com>
2026-06-07 20:29:13 +0700

    feat: 3Sum

--- 3SumTests/ThreeSumTest.cs
+++ 3SumTests/ThreeSumTest.cs
@@ -3,7 +3,18 @@
 public class ThreeSumTest
 {
     [Test]
-    public void three_sum_when_0_0_0_return_one_triplet()
+    public void normal_case()
+    {
+        int[] input = [-1, 0, 1, 2, -1, -4];
+        int[][] expected = [[-1, -1, 2], [-1, 0, 1]];
+
+        var result = ThreeSum(input);
+        
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void case_000()
     {
         int[] input = [0, 0, 0];
         int[][] expected = [[0, 0, 0]];
@@ -14,10 +25,10 @@ public class ThreeSumTest
     }
     
     [Test]
-    public void three_sum_when_all_negative_return_empty()
+    public void case_0000()
     {
-        int[] input = [-4, -1, -1];
-        int[][] expected = [];
+        int[] input = [0, 0, 0, 0];
+        int[][] expected = [[0, 0, 0]];
 
         var result = ThreeSum(input);
         
@@ -25,10 +36,10 @@ public class ThreeSumTest
     }
     
     [Test]
-    public void three_sum_when_all_positive_return_empty()
+    public void case_11()
     {
-        int[] input = [4, 1, 1];
-        int[][] expected = [];
+        int[] input = [-2,0,1,1,2];
+        int[][] expected = [[-2,0,2],[-2,1,1]];
 
         var result = ThreeSum(input);
         
@@ -36,42 +47,47 @@ public class ThreeSumTest
     }
     
     [Test]
-    public void three_sum_when_have_0_return_pair()
+    public void case_22()
     {
-        int[] input = [9, -4, 0, -1, 4, 1, -6];
-        int[][] expected = [[-1, 0, 1], [-4, 0, 4]];
+        int[] input = [-2,0,0,2,2];
+        int[][] expected = [[-2,0,2]];
 
         var result = ThreeSum(input);
         
         Assert.That(result, Is.EqualTo(expected));
     }
     
-    [Test]
-    public void three_sum_when_call_return_triplets()
-    {
-        int[] input = [4, -3, 9, -6, 1, -1];
-        int[][] expected = [[-6, -3, 9], [-3, -1, 4]];
-
-        var result = ThreeSum(input);
-        
-        Assert.That(result, Is.EqualTo(expected));
-    }
-    
-    [Test]
-    public void three_sum_when_have_dupplicated_return_dupplicated()
-    {
-        int[] input = [9, -4, 0, -1, 4, -6, -1, 1, -4];
-        int[][] expected = [[-1, 0, 1], [-1, 0, 1], [-4, 0, -4], [-4, 0, -4]];
-
-        var result = ThreeSum(input);
-        
-        Assert.That(result, Is.EqualTo(expected));
-    }
-    
-    private List<List<int>> ThreeSum(int[] nums)
-    {
+    private List<List<int>> ThreeSum(int[] nums) {
+        Array.Sort(nums);
         var result = new List<List<int>>();
+        for (var i = 0; i < nums.Length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1] || nums[i] > 0)
+            {
+                continue;
+            }
+            
+            var left = i + 1;
+            var right = nums.Length - 1;
+
+            while (left < right) {
+                var sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    result.Add([nums[i], nums[left], nums[right]]);
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                }
+            }
+        }
 
         return result;
     }
+    
 }
